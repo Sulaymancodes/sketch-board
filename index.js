ff1e034471002e392550a221853b0c084aeafcd2
sulaiman-rabiu <sulaymancodes@gmail.com>
2024-02-19 19:45:41 +0100

    Added Settings Container To Control the Sketch UI

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const mainContainer = document.querySelector('.container');
 
-for (let i = 0; i < 100; i++) { 
+for (let i = 0; i < 16; i++) { 
     let divCon = document.createElement('div');
     divCon.classList.add('div-con'); 
 
-    for (let j = 0; j < 100; j++) { 
+    for (let j = 0; j < 16; j++) { 
         let divs = document.createElement('div');
         divs.classList.add('divs'); 
 
