8d440b8b6e888092dcd7beb6cbb1b0a2745f2d54
sulaiman-rabiu <sulaymancodes@gmail.com>
2024-02-20 14:18:54 +0100

    Setting The Grid Color To Black On Mouse Over

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const mainContainer = document.querySelector('.container');
+const resetBtn = document.querySelector('.reset');
+const gridValue = document.querySelector('#grid-value').value;
+const eraserBtn = document.querySelector('.eraser-btn');
+const blackBtn = document.querySelector('.black-btn');
+const rainbowBtn = document.querySelector('.rainbow-btn');
 
-for (let i = 0; i < 16; i++) { 
+
+//Creating the sketch pad container
+for (let i = 0; i < gridValue; i++) { 
     let divCon = document.createElement('div');
     divCon.classList.add('div-con'); 
 
-    for (let j = 0; j < 16; j++) { 
+    for (let j = 0; j < gridValue; j++) { 
         let divs = document.createElement('div');
         divs.classList.add('divs'); 
 
         divCon.appendChild(divs); 
+        
+        //setting the grids to black
+        divs.addEventListener('mouseover', () => {
+            divs.style.backgroundColor = 'black';
+        });
     }
 
     mainContainer.appendChild(divCon);
 }
+
+//setting the height of the skecth pad container 
+//base on the No of Grid
+document.documentElement.style.setProperty('--height-per-grid', `calc(500px / ${gridValue})`);
+
