5db175d16b508a62da270a68c00a5aff8b690a87
sulaiman-rabiu <sulaymancodes@gmail.com>
2024-02-20 17:05:25 +0100

    Added Functionality To Black,Rainbow,Eraser buttons

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
 const mainContainer = document.querySelector('.container');
 const resetBtn = document.querySelector('.reset');
-const gridValue = document.querySelector('#grid-value').value;
+const enterBtn = document.querySelector('.grid-btn');
 const eraserBtn = document.querySelector('.eraser-btn');
 const blackBtn = document.querySelector('.black-btn');
 const rainbowBtn = document.querySelector('.rainbow-btn');
 
 
+const returnGridValue = (function() {
+    return document.querySelector('#grid-value').value;
+})();
+
+
+
 //Creating the sketch pad container
-for (let i = 0; i < gridValue; i++) { 
+for (let i = 0; i < returnGridValue; i++) { 
     let divCon = document.createElement('div');
     divCon.classList.add('div-con'); 
 
-    for (let j = 0; j < gridValue; j++) { 
+    for (let j = 0; j < returnGridValue; j++) { 
         let divs = document.createElement('div');
         divs.classList.add('divs'); 
 
         divCon.appendChild(divs); 
         
-        //setting the grids to black
+        //setting the divs to black
         divs.addEventListener('mouseover', () => {
             divs.style.backgroundColor = 'black';
         });
+
+        //resetting all the divs color to white
+        resetBtn.addEventListener('click', () =>{
+            divs.style.backgroundColor = 'white';
+        });
+
+        //erasing the divs back to white
+        eraserBtn.addEventListener('click', () =>{
+            divs.addEventListener('mouseover', () =>{
+                divs.style.backgroundColor = 'white';
+            })
+        })
+
+        //setting the divs to black
+        blackBtn.addEventListener('click', () =>{
+            divs.addEventListener('mouseover', () =>{
+                divs.style.backgroundColor = 'black';
+            })
+        })
+
+        //setting divs to rainbow colors
+        rainbowBtn.addEventListener('click', () => {
+            document.querySelectorAll('.divs').forEach(div => {
+                div.addEventListener('mouseover', changeToRandomColor);
+            });
+        });
+        
+        function changeToRandomColor(event) {
+            // Generate random RGB values
+            const red = Math.floor(Math.random() * 256); // 0 to 255
+            const green = Math.floor(Math.random() * 256); // 0 to 255
+            const blue = Math.floor(Math.random() * 256); // 0 to 255
+            
+            // Set the background color of the div
+            event.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        }        
     }
 
     mainContainer.appendChild(divCon);
@@ -28,5 +70,5 @@ for (let i = 0; i < gridValue; i++) {
 
 //setting the height of the skecth pad container 
 //base on the No of Grid
-document.documentElement.style.setProperty('--height-per-grid', `calc(500px / ${gridValue})`);
+document.documentElement.style.setProperty('--height-per-grid', `calc(500px / ${returnGridValue})`);
 
